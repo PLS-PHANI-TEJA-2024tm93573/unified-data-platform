e76373ab653e4298f070ede5fa637d0aacb36141
P L S PHANI TEJA <2024tm93573@wilp.bits-pilani.ac.in>
2026-09-13 05:49:59 +0000

    Swagger API-DataService

--- services/data-service/src/health/health.controller.ts
+++ services/data-service/src/health/health.controller.ts
@@ -1,9 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Health')
 @Controller('health')
 export class HealthController {
 
  @Get()
+ @ApiOperation({ summary: 'Check service health' })
+ @ApiResponse({ status: 200, description: 'Service is healthy.', type: Boolean })
  health(){
     return true
  }
